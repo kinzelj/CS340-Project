8b5fc7f2d39bbeffbbfb5c3e61a58caa00d4a37a
kinzelj <kinzelj008@gmail.com>
2019-11-11 22:27:29 -0600

    update app component style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import SideMenu from './SideMenu.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaw } from '@fortawesome/free-solid-svg-icons'
 import '../css/App.css';
 
 class App extends Component {
-
   render() {
     return (
       <div className="main-div">
-        <h2 className="ui block header" id="top-border" style={{ marginTop: "20px" }}>Zoo Management Database</h2>
+        <h2 className="ui block header" id="top-border" style={{ marginTop: "20px" }}>
+          <FontAwesomeIcon icon={faPaw} style={{ width: "25px", height: "25px", border: "1px solid white", borderRadius: "3px"}}/> Zoo Management Database</h2>
         <SideMenu />
       </div>
     );
